c0f187bc2da39a9ffab5f1943c1f5a5118706cc9
murodjon72 <muro21601@gmail.com>
2024-09-19 15:33:17 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,52 @@
+const container = document.querySelector('.container');
+const image = document.querySelector('.image');
+const btnContainer = document.querySelector('.btn_container');
+const btn1 = document.querySelector('.btn1');
+const btn2 = document.querySelector('.btn2');
+const btn3 = document.querySelector('.btn3');
+const btn4 = document.querySelector('.btn4');
+
+
+btn1.addEventListener('click', () => {
+    image.style.backgroundImage = 'url(https://wallpapers.com/images/hd/spectacular-4k-ocean-view-v2cs024q0itgo07y.jpg)'
+    image.style.backgroundSize = '100% 100%'
+})
+
+
+btn2.addEventListener('click', () => {
+    image.style.backgroundImage = 'url(https://static.vecteezy.com/system/resources/previews/024/361/112/non_2x/waterfall-in-a-tropical-jungle-generative-ai-free-photo.jpg)'
+     image.style.backgroundSize = '100% 100%'
+})
+
+
+btn3.addEventListener('click',() => {
+     image.style.backgroundImage = 'url(https://images8.alphacoders.com/111/1118887.jpg)'
+      image.style.backgroundSize = '100% 100%'
+})
+
+
+
+btn4.addEventListener('click',() => {
+    image.style.backgroundImage = 'url(https://static.vecteezy.com/system/resources/thumbnails/039/363/857/small_2x/ai-generated-floral-purple-flower-background-ai-generated-photo.jpg)'
+     image.style.backgroundSize = '100% 100%'
+})
+   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
